7b8098f81be40e54a7e8b7a7068b2d94c0e07ef0
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-04-05 23:52:37 +0900

    [feat] 절대 경로 설정

--- react-todo-list/src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx, css, Global, ClassName } from '@emotion/react';
-
-function App() {
-  return <h1>Hello</h1>;
-}
-
-export default App;
---
+++ react-todo-list/src/App.jsx
@@ -0,0 +1,14 @@
+import { jsx, css, Global, ClassName } from '@emotion/react';
+
+import ToDoHead from '@components/ToDoHead';
+
+function App() {
+  return (
+    <>
+      <ToDoHead />
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
