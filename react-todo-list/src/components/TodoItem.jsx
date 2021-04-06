f8cec5edc8a262eacd31efe3221cf1bcb353158c
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-04-06 10:49:29 +0900

    [feat] todo item ui

--- react-todo-list/src/components/TodoItem.jsx
+++ react-todo-list/src/components/TodoItem.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import { jsx, css } from '@emotion/react';
+import styled from '@emotion/styled/macro';
 
-const TodoItem = () => {
-  return <div>Item</div>;
+import { MdDone, MdDelete } from 'react-icons/md';
+import theme from '@src/theme';
+
+const RemoveBlock = styled.div`
+  width: 32px;
+  height: 32px;
+  display: flex;
+  font-size: 32px;
+  cursor: pointer;
+  color: #dee2e6;
+  &:hover {
+    color: ${theme.redColor};
+  }
+  visibility: hidden;
+`;
+
+const ItemBlock = styled.div`
+  padding: 12px 0;
+  display: flex;
+  align-items: center;
+  &:hover {
+    ${RemoveBlock} {
+      visibility: visible;
+    }
+  }
+`;
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  font-size: 24px;
+  border: 1px solid ${theme.borderColor};
+  border-radius: 50%;
+  margin-right: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid ${theme.mainColor};
+      color: ${theme.mainColor};
+    `};
+`;
+
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: ${theme.checkTextColor};
+    `}
+`;
+
+const TodoItem = ({ done = true }) => {
+  return (
+    <ItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>프로그래밍 학습하기</Text>
+      <RemoveBlock>
+        <MdDelete />
+      </RemoveBlock>
+    </ItemBlock>
+  );
 };
 
 export default TodoItem;
